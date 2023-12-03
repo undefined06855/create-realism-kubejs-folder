1c45d6eade121d76e1713bc4adfa03ca23f83498
undefined06855 <currentcashew57@gmail.com>
2023-12-03 20:22:33 +0000

    done stuff

--- server_scripts/recipes.js
+++ server_scripts/recipes.js
@@ -9,23 +9,19 @@ ServerEvents.recipes(event => {
     // create:andesite_alloy
     event.remove({ output: "create:andesite_alloy" })
     event.recipes.create.mixing("create:andesite_alloy", [
-        "minecraft:andesite",
-        "#forge:nuggets/iron"
-    ])
-    event.recipes.create.mixing("create:andesite_alloy", [
-        "minecraft:andesite",
-        "#forge:ingots/iron"
+        "2x minecraft:andesite",
+        "2x #forge:nuggets/iron"
     ]).heated()
+    event.recipes.create.mixing("4x create:andesite_alloy", [
+        "2x minecraft:andesite",
+        "2x #forge:nuggets/iron"
+    ]).superheated()
 
     // [MOD] pocket_money
+    // might use this eventually but probably won't
     event.remove({ mod: "pocket_money" })
-
-    // exchanging currencies
-    // money shouldn't be renewable - coppper isnt renewable unless you make tunnel bore
-    event.recipes.create.pressing("pocket_money:copper_coin", [ "minecraft:copper_ingot" ])
-    event.recipes.minecraft.smithing("pocket_money:silver_coin", "pocket_money:copper_coin", "create:iron_sheet")
-    event.recipes.minecraft.smithing("pocket_money:gold_coin", "pocket_money:copper_coin", "create:golden_sheet")
 
     // create:belt_connector
     event.remove({ output: "create:belt_connector" })
+    event.recipes.minecraft.blasting("create:belt_connector", "myrtrees:latex", 0, 400)
 })
