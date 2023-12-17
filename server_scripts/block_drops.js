29ddb0b7113b9630b1ae0c5719183ee30407febc
undefined06855 <currentcashew57@gmail.com>
2023-12-17 17:36:50 +0000

    comment

--- server_scripts/block_drops.js
+++ server_scripts/block_drops.js
@@ -1,3 +1,7 @@
+// priority: 20
+// the block drop loot table is broken, so it won't drop resin
+// this fixes that
+
 BlockEvents.broken("better_rubber:full_rubber_log", event => {
     if (Math.random() < 0.5)
         event.block.popItem("better_rubber:resin")
