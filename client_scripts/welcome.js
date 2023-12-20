1563da4cdcc7efa6fb4139a5fba51e4a7d6944eb
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2023-12-20 18:54:27 +0000

    yeah we need a version number before release

--- client_scripts/welcome.js
+++ client_scripts/welcome.js
@@ -1,4 +1,8 @@
 // priority: 0
 
+const VERSION = "1.0"
+const RELEASETYPE = "beta"
+
 console.log("Welcome to Create:Realism's kubejs client-side!")
 console.log("This modpack was made by undefined06855, hope you enjoy!")
+console.log(`VERSION=${VERSION}-${RELEASETYPE}`)
